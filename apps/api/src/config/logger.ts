b5ee4265c6cffef4fdc1bd421bb768dedf7dd028
Steve Higgs <steve@speechify.com>
2025-10-04 14:06:47 +0100

    output to gcp log format in production

--- apps/api/src/config/logger.ts
+++ apps/api/src/config/logger.ts
@@ -1,15 +1,16 @@
 import type { Params } from 'nestjs-pino';
+import { createGcpLoggingPinoConfig } from '@google-cloud/pino-logging-gcp-config';
 
 export const loggerOptions = <Params>{
   pinoHttp: [
     {
       quietReqLogger: true,
       ...(process.env.NODE_ENV === 'production'
-        ? {}
+        ? createGcpLoggingPinoConfig() // output in gcp logging format
         : {
             transport: {
               target: 'pino-pretty',
-              options: { sync: true, singleLine: true },
+              options: { sync: true },
             },
           }),
     },
