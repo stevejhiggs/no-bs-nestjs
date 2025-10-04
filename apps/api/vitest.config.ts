c68fd62fa32daa833014369fd6ace162a5b57857
Steve Higgs <steve@speechify.com>
2025-10-04 11:24:22 +0100

    better support for allowing vite to resolve tsconfig paths
    Use the plugin so we dont need to manually configure aliases
    
    dont need the resolve

--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -1,6 +1,6 @@
-import { resolve } from 'path';
 import swc from 'unplugin-swc';
 import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   test: {
@@ -8,13 +8,8 @@ export default defineConfig({
     root: './',
   },
   plugins: [
+    tsconfigPaths(),
     // This is required to build the test files with SWC
     swc.vite(),
   ],
-  resolve: {
-    alias: {
-      // Ensure Vitest correctly resolves TypeScript path aliases
-      src: resolve(__dirname, './src'),
-    },
-  },
 });
