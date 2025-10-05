ff5192eedea38c599415ff31f8369e4f0e9bdaf0
Steve Higgs <steve@speechify.com>
2025-10-05 12:27:26 +0100

    naming convention

--- apps/api/src/modules/zod-validation/http/dtos/create-user.ts
+++ apps/api/src/modules/zod-validation/http/dtos/create-user.ts
@@ -1,10 +1,10 @@
 import { createZodDto } from 'nestjs-zod';
 import { z } from 'zod';
 
-const CredentialsSchema = z.object({
+const credentialsSchema = z.object({
   username: z.string(),
   email: z.email(),
 });
 
 // class is required for using DTO as a type
-export class CreateUserRequestZodDto extends createZodDto(CredentialsSchema) {}
+export class CreateUserRequestZodDto extends createZodDto(credentialsSchema) {}
